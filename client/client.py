be20c59ceddea77e2b53c2feb586d2c8b08f779e
Manuel Rene Pauls Toews <paulsmrene@live.com>
2021-08-22 22:25:47 -0400

    En vez de usar dos threads, usar dos tasks con asyncio. Obs. requiere ping_interval=0 en servido y ademas bloquea el interfaz de usuario mientras se estan procesando mensajes

--- client/client.py
+++ client/client.py
@@ -1,44 +1,35 @@
 import json
-import queue
 from tkinter.constants import CENTER
 import PySimpleGUI as sg
 import websockets
 import asyncio
 import threading
 
-def main():
+async def main():
     ip_port = connect_menu()
     if ip_port == "":
         return
     window = conectar_hostpital()
 
-    listener_thread = threading.Thread(target=process_incoming, args=(ip_port,))
-    global running, recv_messages, send_messages
-    recv_messages = queue.Queue()
-    send_messages = queue.Queue()
-    running = True
-    listener_thread.start()
+    await listener(ip_port)
 
-    #conectar
-    while True: #Event Loop
-        window, event, values = sg.read_all_windows(timeout=100)
-        if event == sg.WIN_CLOSED or event == 'Salir':
-            running = False
-            listener_thread.join()
-            window.close()
-            break
+async def gui_producer():
+    window, event, values = sg.read_all_windows(timeout=100)
+    if event == sg.WIN_CLOSED or event == 'Salir':
+        window.close()
+        return "close"
 
-        #Eventos de hospital
-        elif event == 'Ver Estado':
-            send_messages.put(json.dumps())
-        elif event == 'Agregar Cama':  
-            agregar_cama()
-        elif event == 'Eliminar Cama': 
-            eliminar_cama(values["-ID-"])
-        elif event == 'Ocupar Cama': 
-            ocupar_cama(values["-ID-"])
-        elif event == 'Desocupar Cama':
-            desocupar_cama(values["-ID-"])
+    #Eventos de hospital
+    elif event == 'Ver Estado':
+        return json.dumps({"operation": 1})
+    elif event == 'Agregar Cama':  
+        agregar_cama()
+    elif event == 'Eliminar Cama': 
+        eliminar_cama(values["-ID-"])
+    elif event == 'Ocupar Cama': 
+        ocupar_cama(values["-ID-"])
+    elif event == 'Desocupar Cama':
+        desocupar_cama(values["-ID-"])
 
 def conectar_hostpital():
     layout = [
@@ -67,27 +58,30 @@ def connect_menu():
             window.close()
             return values["-IP_PORT-"]
         
-        while recv_messages.qsize() != 0:
-            message = recv_messages.get()
-            print(message)
 
-def process_incoming(ip_port):
-    asyncio.run(listener(ip_port))
 async def listener(ip_port):
     websocket = await conectar(ip_port)
-    print(running)
-    while running:
-        try:
-            message = await asyncio.wait_for(websocket.recv(), timeout=1)
-            recv_messages.put(message)
-        except asyncio.exceptions.TimeoutError:
-            pass
-        
-        while send_messages.qsize() > 0:
-            try:
-                await websocket.send(send_messages.get())
-            except asyncio.exceptions.TimeoutError:
-                print("Error enviando mensaje!")
+    while True:
+        listener_task = asyncio.ensure_future(websocket.recv())
+        producer_task = asyncio.ensure_future(gui_producer())
+        done, pending = await asyncio.wait(
+            [listener_task, producer_task],
+            return_when = asyncio.FIRST_COMPLETED)
+
+        if listener_task in done:
+            message = listener_task.result()
+            print(message)
+        else:
+            listener_task.cancel()
+
+        if producer_task in done:
+            message = producer_task.result()
+            if message == "close":
+                break
+            if message:
+                await websocket.send(message)
+        else:
+            producer_task.cancel()
     await websocket.close()
     
 
@@ -110,10 +104,10 @@ def desocupar_cama(id):
 async def conectar(ip_port):
     try:
         uri = f"ws://{ip_port}/"
-        websocket = await websockets.connect(uri)
+        websocket = await websockets.connect(uri, ping_interval=None)
         return websocket
     except ConnectionRefusedError:
         print("Connection error")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
