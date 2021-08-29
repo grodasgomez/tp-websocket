275a6256a1ada140c44b1a455f18a088da677d87
frandepy2 <frandepy@fpuna.edu.py>
2021-08-29 16:51:47 -0400

    bugfix correccion de nombre de variable que generaba error

--- client/client.py
+++ client/client.py
@@ -136,7 +136,7 @@ def actualizarPant(window):
 
 
 async def listener(websocket, window, bedList):
-    filer = ""
+    filtro = ""
     auxBedList = bedList
     while True:
         # Crear dos tasks que en el mismo thread se encargan de esperar nuevos mensajes y leer gui
@@ -156,7 +156,7 @@ async def listener(websocket, window, bedList):
                 if operation == 1:
                     bedList = message["data"]
                 elif operation == 2:
-                    if (filter == "" or message["data"]['hospitalId']==filter):
+                    if (filtro == "" or message["data"]['hospitalId']==filtro):
                         auxBedList.append(message["data"])
                     bedList.append(message["data"])
                 elif operation == 3:
@@ -180,14 +180,14 @@ async def listener(websocket, window, bedList):
             if message == "DESCONECTAR" or message == "SALIR":  # Si se quiere terminar la aplicación
                 return message
             elif message == "DESFILTRAR":
-                filter=""
+                filtro=""
                 auxBedList=bedList
                 actualizarPant(window)
                 window = pantHospital(auxBedList)     
             elif message:  # Solo hacer algo si hubo input del usuario
                 if 'filter' in message:
                     message = json.loads(message)
-                    filter = message['hospitalId']
+                    filtro = message['hospitalId']
                     auxBedList = []
                     for bed in bedList:
                         if bed['hospitalId']==message['hospitalId']:
